5e3d2544f650f726303cbc32a70662717a522788
Adeyinka <fawazmusty247@gmail.com>
2025-01-08 02:58:06 +0100

    oluwa oshe

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ return(
 <>
 
     <div className='contact'>
-      <div style={{display:"flex", gap:"1em", justifyContent:"center"}}>
+      <div style={{display:"flex", gap:"1.3em", justifyContent:"center"}}>
 <Link  href='https://www.linkedin.com/in/fawas-ajibade-80aa56255' target="_blank" rel="noopener noreferrer"><LinkedInIcon style={{fill:'#0c2461', fontSize: '40px'}}/> </Link> 
  <Link href='https://twitter.com/okin_tech' target="_blank" rel="noopener noreferrer"> <XIcon style={{fill:'#0c2461', fontSize: '40px'}}/> </Link> 
   <Link href='https://github.com/yinkaToTheWorld' target="_blank" rel="noopener noreferrer"> <GitHubIcon style={{fill:'#0c2461', fontSize: '40px'}}/> </Link>  
